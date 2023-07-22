daa10c6ea1290b673cef2dd288b77e2416a9ef13
AlpGreva <128251911+AlpGreva@users.noreply.github.com>
2023-07-22 02:17:18 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -74,5 +74,18 @@ then
 	export QUEUE_BULL_REDIS_PASSWORD=$N8N_REDIS_PASSWORD
 fi
 
+FROM nginx:alpine
+ 
+COPY nginx.conf /etc/nginx/conf.d/configfile.template
+COPY --from=builder /app/dist /usr/share/nginx/html
+ 
+ENV \
+    PORT=8080 \
+    HOST=0.0.0.0
+ 
+EXPOSE 8080
+ 
+CMD sh -c "envsubst '\$PORT' < /etc/nginx/conf.d/configfile.template > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'"
+
 # kickstart nodemation
 n8n
